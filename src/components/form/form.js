7490cca525610b28e36428882d3bd6d3ed25e673
naveen <185893220+codamee@users.noreply.github.com>
2026-02-25 08:57:35 +0530

    added printui and getformData funcs

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -10,25 +10,25 @@ function form() {
              <form action="/">
                 <div>
                     <label for="title">Title</label>
-                    <input type="text" name="title" id="title" />
+                    <input type="text" name="title" id="title" required/>
                 </div>
                 <div>
                     <label for="discription">Discription</label>
-                    <input type="text" name="discription" id="discription" />
+                    <input type="text" name="discription" id="discription" required/>
                 </div>
                 <div>
-                    <label for="date">Due-Time</label>
-                        <input type="date" name="date" id="date" />
+                    <label for="duetime">Due-Time</label>
+                        <input type="datetime-local" name="duetime" id="date" required/>
                 </div>
                 <div>
                     <label for="priority">Priority</label>
-                    <select name="priority" id="priority">
+                    <select name="priority" id="priority" required>
                             <option value="Top">Top</option>
                             <option value="Medium">Medium</option>
                             <option value="Least">Least</option>
                     </select>
                 </div>
-                <button type=button id=submitBtn >Add Project</button>
+                <button id="submitBtn" >Add Project</button>
             </form>
         </div>
     `
--- src/index.js
+++ src/index.js
@@ -3,4 +3,45 @@ import { Project, Manager } from "./components/array/array.js"
 import { form } from "./components/form/form.js"
 
 
+let addTodoBtn = document.querySelector(".addTodo")
+let formContainer = document.querySelector(".formContainer")
+addTodoBtn.addEventListener("click", (elem) => {
+    formContainer.style.visibility = "visible"
 
+})
+let project = new Project("default")
+function getFormData() {
+    let submitBtn = document.querySelector('#submitBtn')
+    let formElem = document.querySelector('form')
+
+    submitBtn.addEventListener("click", (elem) => {
+        elem.preventDefault()
+        formContainer.style.visibility = "hidden"
+        let formData = new FormData(formElem)
+        let data = Object.fromEntries(formData.entries());
+        project.setArray(data.title, data.discription, data.duetime, data.priority)
+        printUi()
+    })
+}
+
+function printUi() {
+    let container = document.querySelector(".content")
+    container.innerHTML = ``
+    project.getArray().forEach(elem => {
+        let divElem = document.createElement("div")
+        divElem.classList.add("todo")
+        divElem.innerHTML =/*html*/
+            `
+        <div class="title">${elem.title}</div>
+        <div class="discription">${elem.discription}</div>
+        <div class="duetime">${elem.duetime}</div>
+        <div class="priority">${elem.priority}</div>
+        <div class="btns">
+            <div class="edit">EditBtn</div>
+            <div class="del">DelBtn</div>
+        </div>
+        `
+        container.append(divElem)
+    })
+}
+getFormData()
