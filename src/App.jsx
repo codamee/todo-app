5e09069f3a96b7ac2f889afa608230ac66b2e188
naveen <185893220+codamee@users.noreply.github.com>
2026-03-17 13:08:42 +0530

    ui:add reponsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,15 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className='text-slate-500 flex flex-col gap-4 w-full md:w-[50%] md:mx-auto bg-slate-300 p-4 md:m-4 md:rounded-sm'>
-        <div className='flex w-full border-b py-6'>
-          <input className='border px-4 py-2 w-[60%]  md:w-[80%]' type="text" placeholder='Add your todo here...' name="todoText" id="todoText" />
-          <button className='bg-slate-950 md:w-[20%] w-60% cursor-pointer text-slate-50 px-4 py-2  hover:bg-slate-800'>Add Todo</button>
+      <div className='text-slate-600 box-border  flex flex-col gap-4 w-full md:w-[50%] md:mx-auto bg-slate-300 px-4 md:m-4 md:rounded-sm'>
+        <div className='flex flex-col md:flex-row w-full border-b py-6 gap-2'>
+          <input className='border focus:outline-0 focus:border-cyan-700 px-4 py-2  md:w-[80%]' type="text" placeholder='Add your todo here...' name="todoText" id="todoText" />
+          <button className='bg-slate-950 md:w-[20%]  cursor-pointer text-slate-50 px-4 py-2  hover:bg-slate-800'>Add Todo</button>
         </div>
         <div className='flex flex-col gap-3'>
           <h2>Your Todos</h2>
-          <div className='todo-container'>
+          <div className='todo-container flex flex-col gap-2 overflow-auto  h-125'>
+            <Todo />
             <Todo />
           </div>
         </div>
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -6,7 +6,7 @@ const Todo = () => {
         <div className="todo flex justify-between items-center bg-slate-200 px-4 py-2 rounded-sm">
             <div className=' flex gap-3'>
                 <input className='w-4 cursor-pointer' type="checkbox" name="check" id="check" />
-                <p>Read a book in the evening</p>
+                <p className='text-sm md:text-'lg>Read a book in the evening</p>
             </div>
             <div className="edit flex gap-4">
                 <FaEdit className='cursor-pointer text-xl' />
