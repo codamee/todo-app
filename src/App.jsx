6f811122b981b275e1ec417584623bfd8e953b4a
naveen <185893220+codamee@users.noreply.github.com>
2026-03-17 12:02:44 +0530

    ui:added the app ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import Navbar from './components/Navbar'
+import Todo from './components/Todo';
+
 import { useState } from 'react'
 import './App.css'
 
@@ -8,7 +10,18 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className='text-slate-500 font-bold'>Hello world</div>
+      <div className='text-slate-500 flex flex-col gap-4 w-full md:w-[50%] md:mx-auto bg-slate-300 p-4 md:m-4 md:rounded-sm'>
+        <div className='flex w-full border-b py-6'>
+          <input className='border px-4 py-2 w-[60%]  md:w-[80%]' type="text" placeholder='Add your todo here...' name="todoText" id="todoText" />
+          <button className='bg-slate-950 md:w-[20%] w-60% cursor-pointer text-slate-50 px-4 py-2  hover:bg-slate-800'>Add Todo</button>
+        </div>
+        <div className='flex flex-col gap-3'>
+          <h2>Your Todos</h2>
+          <div className='todo-container'>
+            <Todo />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
